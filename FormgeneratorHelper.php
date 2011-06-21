9d957a6e9cf7ea5333d344b3a35b4cad79157073
Konrad Abicht <k.abicht@googlemail.com>
2011-06-21 12:19:52 +0200

    - chg: show "New instance" tab only if current controller = resource or formgenerator

--- FormgeneratorHelper.php
+++ FormgeneratorHelper.php
@@ -9,23 +9,27 @@ class FormgeneratorHelper extends OntoWiki_Component_Helper
     public function __construct()
     {
         $owApp = OntoWiki::getInstance();
+        $request = Zend_Controller_Front::getInstance()->getRequest();
+        $c = $request->getControllerName();
         
         // If a model has been selected
-        if ($owApp->selectedModel != null) {
+        if ($owApp->selectedModel != null 
+        
+            AND ( 'resource' == $c OR 'formgenerator' == $c )
             
             // A class was selected
-            if ( -1 !== OntoWiki_Model_Instances::getSelectedClass () )
-            {
-                // Add entry in tab list
-                OntoWiki_Navigation::register (
-                    'formgenerator_form', 
-                    array(
-                        'controller' => 'formgenerator', 
-                        'action'     => 'form', 
-                        'name'       => 'New Instance'
-                    )
-                );
-            }
+            AND -1 !== OntoWiki_Model_Instances::getSelectedClass () ) {
+             
+            
+            // Add entry in tab list
+            OntoWiki_Navigation::register (
+                'formgenerator_form', 
+                array(
+                    'controller' => 'formgenerator', 
+                    'action'     => 'form', 
+                    'name'       => 'New Instance'
+                )
+            );
         }
     }
 }
