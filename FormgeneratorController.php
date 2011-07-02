e588414eba8f9fe09018bf266bbe2bfb3ee4b53c
Lars Eidam <larseidam@googlemail.com>
2011-07-02 20:59:16 +0200

    - add: function editTriple to edit a triple in store (delete the old one and add the new one) - chg: sendform Action to edit form entries - chg: now it is possible to edit all predicates but only they were directly added befor - attention: if predicates were edit all triples of the form will be deleted and than all triples were added again, even those they have not chanced

--- FormgeneratorController.php
+++ FormgeneratorController.php
@@ -31,7 +31,6 @@ class FormgeneratorController extends OntoWiki_Controller_Component
     {   
         // TODO Implement stuff for showing unfilled mandatory fields.
         
-        
         // Build URL string for formula
         $this->view->actionUrl = (string)   
                                  new OntoWiki_Url ( 
@@ -115,22 +114,39 @@ class FormgeneratorController extends OntoWiki_Controller_Component
         $json['triples'] = Array();
         $json['relations'] = Array();
         
+        $modus = 'add';
+        
         // TODO How to merge targetclasses and labelparts ?!
         $labelParts = Tools::getLabelParts ( $checkingForm );
         
         $targetClasses = Tools::getTargetClasses ( $checkingForm );
-               
+        
+        
         // Creating resources from target classes.
         $resourceArray = array ();
         
-        foreach ( $targetClasses as $targetClass ) 
+        if (isset($_REQUEST['isedit']) && "" != $_REQUEST['isedit'] && 'true' == $_REQUEST['isedit'])
         {
-            $resourceArray [ $targetClass ] = Tools::generateUniqueUri ( 
-                (string) $this->_owApp->selectedModel, 
-                $targetClass, 
-                'foobar' // TODO Use labelparts!
-            );
+            foreach ( $targetClasses as $targetClass ) 
+            {
+                $resourceArray [ $targetClass ] = $_REQUEST[$targetClass];
+            }
+            $modus = 'edit';
         }
+        else
+        {
+            foreach ( $targetClasses as $targetClass ) 
+            {
+                $resourceArray [ $targetClass ] = Tools::generateUniqueUri ( 
+                    (string) $this->_owApp->selectedModel, 
+                    $targetClass, 
+                    'foobar' // TODO Use labelparts!
+                );
+            }
+            $json['resources'] = $resourceArray;
+        }
+        
+        
         
         //echo "<br>Create following triples:";
         
@@ -152,14 +168,20 @@ class FormgeneratorController extends OntoWiki_Controller_Component
                         $triple['O'] = $_REQUEST [$entry ['md5']];
                         $triple['md5'] = $entry ['md5'];
                         $json['triples'][] = $triple;
-                        Erfurt_App::getInstance()
-                            ->getStore()
-                            ->addStatement (
-                                'http://als.dispedia.info/', 
-                                $resourceArray [ $class ] . '#debug',
-                                $entry ['predicateuri'], 
-                                array ( 'value' => $_REQUEST [$entry ['md5']], 'type' => '')
-                            );
+                        
+                        if(!$this->editTriple(   $resourceArray [ $class ] . '#debug',
+                                                $entry ['predicateuri'],
+                                                null,
+                                                $_REQUEST [$entry ['md5']],
+                                                'literal',
+                                                $modus))
+                        {
+                            $json['error'] = Array();
+                            $json['error']['message'] = 'add/edit triple';
+                            $json['error']['subject'] = $triple['S'];
+                            $json['error']['predicate'] = $triple['P'];
+                            $json['error']['object'] = $triple['O'];
+                        }
                     }
                 }
             }
@@ -190,14 +212,19 @@ class FormgeneratorController extends OntoWiki_Controller_Component
                     $relation_array['O'] = $resourceArray [ $entry ['targetclass'] ];
                     $json['relations'][] = $relation_array;
                     
-                   Erfurt_App::getInstance()
-                    ->getStore()
-                    ->addStatement (
-                        'http://als.dispedia.info/',
-                        $resourceArray [ $targetClasses [0] ] . '#debug',
-                        $relation,
-                        array ( 'value' => $resourceArray [ $entry ['targetclass'] ], 'type' => 'uri' )
-                    );
+                    if (!$this->editTriple(   $relation_array['S'],
+                                            $relation_array['P'],
+                                            null,
+                                            $relation_array['O'],
+                                            'uri',
+                                            $modus))
+                    {
+                        $json['error'] = Array();
+                        $json['error']['message'] = 'add/edit relation';
+                        $json['error']['subject'] = $relation_array['S'];
+                        $json['error']['predicate'] = $relation_array['P'];
+                        $json['error']['object'] = $relation_array['O'];
+                    }
                 }
             }
         }
@@ -257,10 +284,74 @@ class FormgeneratorController extends OntoWiki_Controller_Component
             // Default: xsd:string ( A simple textfield ) 
             default: 
                 
-                return '<input type="text" id="'. $fieldName .'" name="'. $fieldName .'">';
+                return '<input type="text" id="'. $fieldName .'" name="'. $fieldName .'" value="Test01">';
             
                 break;
         }
+    }
+    
+    /**
+     * Chance a triple/relation in store, by delete the old one and add
+     * the new one
+     * @param oldSubjectUri old subject URI
+     * @param oldPredicateUri old predicate URI
+     * @param oldObject old object URI or literal
+     * @param newObject new object URI or literal
+     * @param type object type ('uri' or 'literal')
+     * @return true if edit successful, else false
+     */
+    private function editTriple($subjectUri,
+                                $predicateUri,
+                                $oldObject,
+                                $newObject,
+                                $type,
+                                $modus)
+    {
+        // get store
+        $store = Erfurt_App::getInstance()->getStore();
+        
+        $options = Array();
+        
+        if ('uri' == $type)
+        {
+            $options['object_type'] = Erfurt_Store::TYPE_IRI;
+        }
+        else if ('literal' == $type)
+        {
+            $type == '';
+            $options['object_type'] = Erfurt_Store::TYPE_LITERAL;
+        }
+        else
+            return false;
+    
+        // only for sigilton predicates, deletes all triples with subject
+        // and predicate and ignore the object
+        // TODO: make it posible for mutliple predicates of the them type
+        if ('edit' == $modus)
+        {
+            $deletedTriples = $store->deleteMatchingStatements (
+                    (string) $this->_owApp->selectedModel,
+                    $subjectUri,
+                    $predicateUri,
+                    null, //$oldObject,
+                    $options
+            );
+            if (1 != $deletedTriples)
+                return false;
+        }
+        if (null != $newObject)
+        {
+            $addedTriples = $store->addStatement (
+                (string) $this->_owApp->selectedModel, 
+                $subjectUri,
+                $predicateUri, 
+                array ( 'value' => $newObject, 'type' => $type)
+            );
+            if (1 != $addedTriples)
+                return false;
+        }
+        
+        return true;
     }
     
     /**
